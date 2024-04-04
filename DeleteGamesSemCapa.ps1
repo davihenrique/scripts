561b8e6528e263906bb2255460aa588e7a7d8294
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-04-04 20:54:14 -0300

    Add: DeleteGamesSemCapa.ps1

---
+++ DeleteGamesSemCapa.ps1
@@ -0,0 +1,18 @@
+﻿foreach ($fileGame in (Get-ChildItem -Exclude Imgs) )
+{
+    [bool] $myval = $false
+    foreach ($fileImg in (Get-ChildItem -Path '.\Imgs').Basename )
+    {
+        if($fileGame.Basename.Equals($fileImg))
+        {
+            $myval = $true
+        }       
+    }
+
+    if($myval.Equals($false))
+    {
+        Remove-Item $fileGame
+        Write-Host $fileGame.Basename
+    }
+
+}
